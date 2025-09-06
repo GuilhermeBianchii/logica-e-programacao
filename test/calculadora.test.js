c9c1c22ab1f9e1559bfef267ac98b906a0602bcc
GUILHERME BIANCHI <Guimultimarcas18@gmail.com>
2025-09-06 09:46:22 -0300

    Acrescentei testes de somar valores zero e valores negativos

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -30,4 +30,25 @@ describe('Testes da Função de Soma', function(){
 
     });
 
+    it('A função deve ser capaz de somar dois números zerados', function () {
+        //Coleta o resultado da função
+        const resultadoDaSoma = somarDoisnumeros(0,0);
+
+        //Comparar o resultado com o valor que você espera
+
+        expect(resultadoDaSoma).to.equal(0);
+
+    });
+
+    it('A função deve ser capaz de somar dois números negativos', function (){
+        //Coleta o resultado da função
+
+        const resultadoDaSoma =somarDoisnumeros (-500,-200);
+
+        //Comparar o resultado com o valor que você espera
+
+        expect(resultadoDaSoma).to.equal(-700);
+
+    });
+
 });
